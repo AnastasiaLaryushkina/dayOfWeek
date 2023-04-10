472e5392ccca1089c9674f5e475c65d1f76a2ba0
AnastasiaLaryushkina <alaryushkina@gmail.com>
2023-04-10 15:53:51 +0300

    Задача Maven

---
+++ src/main/java/org/example/Weekday.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public enum Weekday {
+
+    SUNDAY ("Сегодня воскресенье"),
+    MONDAY ("Сегодня понедельник"),
+    TUESDAY ("Сегодня вторник"),
+    WEDNESDAY ("Сегодня среда"),
+    THURSDAY ("Сегодня четверг"),
+    FRIDAY ("Сегодня пятница"),
+    SATURDAY ("Сегодня суббота");
+
+    private String message;
+    Weekday(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
